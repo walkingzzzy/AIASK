e9ee2d337c994f8c7c45ec5ba35fbc232ea2fb60
walkingzzzy <130951969+walkingzzzy@users.noreply.github.com>
2026-02-11 19:14:43 +0800

    chore: remove chat-client package (deprecated Electron/Web UI)

--- packages/chat-client/src/renderer.ts
+++
@@ -1,5 +0,0 @@
-/**
- * 渲染进程入口 - 指向 React 应用
- */
-
-import './renderer/index';
--- packages/chat-client/src/renderer/api/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * API 模块导出
- */
-
-export * from './api';
-export { createElectronAdapter } from './electron-adapter';
-export { createWebAdapter } from './web-adapter';
